4d3c10f370859059a3a98b1e518defc787ab8d30
Andrew Rice <andrewrice90@gmail.com>
2014-05-06 17:48:36 +0100

    final update

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,5 +2,5 @@ class Vehicle < ActiveRecord::Base
 	belongs_to :user
 	mount_uploader :carImageUploader, ImageUploader
 
-	validates :height,  :numericality => { :greater_than => 0 }
+	validates :height,  :numericality => { :greater_than => 0, :less_than => 7}
 end
